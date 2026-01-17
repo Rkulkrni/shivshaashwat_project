d1b3e7ec687a445fe9efd2fd5ee810d0a83d1f3a
Rkulkrni <rahulspk7@gmail.com>
2026-01-17 18:52:27 +0530

    * project related all data files where all dynamic data is there.

---
+++ data/partners.js
@@ -0,0 +1,44 @@
+const partners = [
+  {
+    name: "Dr. Shivvkant Patil (MD)",
+    org: "Vighneshwar Ayurved Hospital",
+    address: "Sangli",
+    image: "assets/images/logo/partner_logo/hospital_default_img.png",
+  },
+  {
+    name: "Dr. Pravin Gore (MD)",
+    org: "Sai Hospital",
+    address: "Phondshiras, Solapur-413109",
+    image: "assets/images/logo/partner_logo/hospital_default_img.png",
+  },
+  {
+    name: "Dr. Ashwini Khot",
+    org: "Hospital Name",
+    address: "Maharashtra",
+    image: "assets/images/logo/partner_logo/hospital_default_img.png",
+  },
+  {
+    name: "Dr. Mahesh Kare",
+    org: "Kare child hospital",
+    address: "Natepute",
+    image: "assets/images/logo/partner_logo/hospital_default_img.png",
+  },
+  {
+    name: "Dr. Bhiva Bhale",
+    org: "Bhale hospital",
+    address: "Natepute",
+    image: "assets/images/logo/partner_logo/hospital_default_img.png",
+  },
+  {
+    name: "Dr. Arpana Dongare",
+    org: "Hospital",
+    address: "Maharashtra",
+    image: "assets/images/logo/partner_logo/hospital_default_img.png",
+  },
+  {
+    name: "Dr. Samarjita Thorat",
+    org: "Hospital",
+    address: "Maharashtra",
+    image: "assets/images/logo/partner_logo/hospital_default_img.png",
+  },
+];
